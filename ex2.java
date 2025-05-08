21d01396963db7913e01a18433ba1b10c12aa2ed
MD <manardg09@gmail.com>
2025-05-08 11:51:26 +0100

    ex2.java

--- ex2.java
+++ ex2.java
@@ -1,47 +1,48 @@
-import java.util.Arrays;
-
-public class ex2 {
+public class Vecteur {
+    private double[] elements;
     
-    class Vecteur {
-        private final double[] composantes;
-
-        public double[] getComposantes() {
-            return composantes;
+    // Crée un vecteur avec des valeurs
+    public Vecteur(double... valeurs) {
+        this.elements = valeurs;
+    }
+    
+    // Additionne avec un autre vecteur
+    public Vecteur ajouter(Vecteur autre) {
+        double[] somme = new double[elements.length];
+        for (int i = 0; i < elements.length; i++) {
+            somme[i] = elements[i] + autre.elements[i];
         }
-
-        // Constructeur
-        public Vecteur(double... composantes) {
-            this.composantes = composantes;
+        return new Vecteur(somme);
+    }
+    
+    // Calcule le produit scalaire
+    public double produitScalaire(Vecteur autre) {
+        double resultat = 0;
+        for (int i = 0; i < elements.length; i++) {
+            resultat += elements[i] * autre.elements[i];
         }
-
-        // Produit scalaire
-        public double produitScalaire(Vecteur autre) {
-            if (this.composantes.length != autre.composantes.length) {
-                return 0;
-            }
-            double resultat = 0;
-            for (int i = 0; i < this.composantes.length; i++) {
-                resultat += this.composantes[i] * autre.composantes[i];
-            }
-            return resultat;
+        return resultat;
+    }
+    
+    // Affiche le vecteur
+    public void afficher() {
+        for (double val : elements) {
+            System.out.print(val + " ");
         }
-
-        // Addition de vecteurs
-        public Vecteur addition(Vecteur autre) {
-            if (this.composantes.length != autre.composantes.length) {
-                return null;
-            }
-
-            double[] somme = new double[this.composantes.length];
-            for (int i = 0; i < this.composantes.length; i++) {
-                somme[i] = this.composantes[i] + autre.composantes[i];
-            }
-            return new Vecteur(somme);
-        }
-
-        // Affichage
-        public void afficher() {
-            System.out.println(Arrays.toString(this.composantes));
-        }
+        System.out.println();
+    }
+    
+    // Exemple d'utilisation
+    public static void main(String[] args) {
+        Vecteur v1 = new Vecteur(1, 2, 3);
+        Vecteur v2 = new Vecteur(4, 5, 6);
+        
+        System.out.print("Vecteur 1: "); v1.afficher();
+        System.out.print("Vecteur 2: "); v2.afficher();
+        
+        Vecteur somme = v1.ajouter(v2);
+        System.out.print("Somme: "); somme.afficher();
+        
+        System.out.println("Produit scalaire: " + v1.produitScalaire(v2));
     }
 }
